bc0ff5103e8dac97518319be3dd5d528a79df3b9
enrique-dealba <enrique.dealba@e-o.solutions>
2023-10-26 10:24:00 -0500

    Update my_server.py
    LLMEngine, EngineArgs used - will be testing how this works in vLLM.

--- my_server.py
+++ my_server.py
@@ -4,19 +4,32 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, Response
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.sampling_params import SamplingParams
+from vllm import LLMEngine, EngineArgs
 
 app = FastAPI()
 
 # Initialize engine with a default model
 opt_model = "facebook/opt-125m"
 mistral_model = "mistralai/Mistral-7B-Instruct-v0.1"
-engine = AsyncLLMEngine(model=opt_model, worker_use_ray=False, engine_use_ray=False)
+engine_args = EngineArgs(model="facebook/opt-125m")
+engine = LLMEngine.from_engine_args(engine_args)
 
 @app.post("/custom_generate")
 async def custom_generate(request: Request):
+    request_id = 0
     request_dict = await request.json()
     text = request_dict.pop("text", None)
     sampling_params = SamplingParams(**request_dict)
+    params_2 = SamplingParams(n=2, # Number of output sequences to return for the given prompt
+                              temperature=0.1,
+                              top_p=0.95, # Must be in (0, 1] - set to 1 to consider all tokens
+                              frequency_penalty=0.1, # > 0 leads to new tokens, < 0 leads to repeat tokens
+                              use_beam_search=False, # whether to use beam search instead of sampling
+                              # max_tokens=256,
+                            )
+    
+    # prompt, sampling_params = test_prompts.pop(0)
+    # engine.add_request(str(request_id), prompt, sampling_params)
 
     results = await engine.generate([text], sampling_params)
     
