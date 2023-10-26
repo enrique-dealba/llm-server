9af2363deccd00779bf79f086f5c53b2d9046e3a
enrique-dealba <enrique.dealba@e-o.solutions>
2023-10-26 12:04:48 -0500

    Update my_server.py
    Using AsyncEngines for LLMs

--- my_server.py
+++ my_server.py
@@ -17,8 +17,8 @@ mistral_model = "mistralai/Mistral-7B-Instruct-v0.1"
 # Set model directly
 opt_model = os.getenv("MODEL", "facebook/opt-125m")
 
-engine_args = EngineArgs(model=opt_model)
-engine = LLMEngine.from_engine_args(engine_args)
+engine_args = AsyncEngineArgs(model=opt_model)
+engine = AsyncLLMEngine.from_engine_args(engine_args)
 print("Engine initialized")  # Debugging line
 
 @app.post("/generate")
