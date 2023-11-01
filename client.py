68d15d8148daa10f5d5c1b59d03b710de663a84b
enrique-dealba <enrique.dealba@e-o.solutions>
2023-11-01 15:50:25 -0500

    Parsing LLM output part 2
    Parsing LLM output part 2

--- client.py
+++ client.py
@@ -16,6 +16,17 @@ def parse_llm(response):
     print(type(response))
     return response
 
+def parse_llm(response):
+    raw_text = response.get('text', [])
+    if not raw_text:
+        return "No response from LLM."
+    
+    raw_text = ' '.join(raw_text)  # Join list to a single string if needed
+    lines = raw_text.split('\n')  # Split by newline to get individual lines
+    parsed_str = "\n".join([line.strip() for line in lines])
+    
+    return parsed_str.strip()
+
 if __name__ == "__main__":
     prompt = input("Prompt: ")
     result = generate_text(prompt)
