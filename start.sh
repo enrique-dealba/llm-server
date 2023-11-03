6958ede8cfc78cc17c4d3d949181cae37a118cda
enrique-dealba <enrique.dealba@e-o.solutions>
2023-11-03 13:02:56 -0500

    Zephyr test
    Zephyr test for Zephyr 7B

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@
 export $(grep -v '^#' .env | xargs)
 
 # Start FastAPI server
-uvicorn my_server:app --host 0.0.0.0 --port 8888
+# uvicorn my_server:app --host 0.0.0.0 --port 8888
 uvicorn llm_server:app --host 0.0.0.0 --port 8888
