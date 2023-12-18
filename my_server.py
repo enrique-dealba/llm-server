7b5a1f0ba9e8a4ade202d61a320a149ca5a9886a
enrique-dealba <enrique.dealba@e-o.solutions>
2023-12-18 14:46:44 -0600

    Code Cleanup
    Code Cleanup (cont)

--- my_server.py
+++ my_server.py
@@ -1,29 +1,24 @@
-import argparse
-import json
 import os
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, Response, StreamingResponse
-from vllm import EngineArgs, LLMEngine, SamplingParams
+from vllm import SamplingParams
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.utils import random_uuid
 
+from config import DEFAULT_MODEL, MAX_TOKENS, NUM_RESPONSES, TEMPERATURE, TOP_P
+
 app = FastAPI()
 
-opt_model: str = "facebook/opt-125m"
-mistral_model: str = "mistralai/Mistral-7B-Instruct-v0.1"
-mistral_cpu: str = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF" # Doesn't work
-mistral_awq: str = "TheBloke/Mistral-7B-Instruct-v0.1-AWQ"
-
-zephyr_model: str = "HuggingFaceH4/zephyr-7b-beta"
-hermes_model: str = "teknium/OpenHermes-2.5-Mistral-7B"
-
 # Setting model directly
-llm_model = os.getenv("MODEL", mistral_model)
+llm_model = os.getenv("MODEL", DEFAULT_MODEL)
+
+GPU_UTILIZATION = 0.30
 
 # gpu_memory_utilization=0.25 works for 7B models
-engine_args = AsyncEngineArgs(model=llm_model, gpu_memory_utilization=0.30)
+engine_args = AsyncEngineArgs(model=llm_model,
+                              gpu_memory_utilization=GPU_UTILIZATION)
 engine = AsyncLLMEngine.from_engine_args(engine_args)
 
 @app.post("/generate")
@@ -31,12 +26,10 @@ async def generate(request: Request):
     request_dict = await request.json()
     prompt = request_dict.pop("text", None)
 
-    sampling_params = SamplingParams(n=1, # Number of output sequences to return for the given prompt
-                              temperature=0.2,
-                              top_p=0.95, # Must be in (0, 1] - set to 1 to consider all tokens
-                              #frequency_penalty=0.1, # > 0 leads to new tokens, < 0 leads to repeat tokens
-                              #use_beam_search=False, # whether to use beam search instead of sampling
-                              max_tokens=500,
+    sampling_params = SamplingParams(n=NUM_RESPONSES,
+                              temperature=TEMPERATURE,
+                              top_p=TOP_P,
+                              max_tokens=MAX_TOKENS,
     )
 
     request_id = random_uuid()
@@ -47,7 +40,6 @@ async def generate(request: Request):
     async for request_output in results_generator:
         if await request.is_disconnected():
             print("Client disconnected. Aborting request.")
-            # Abort the request if the client disconnects.
             await engine.abort(request_id)
             return Response(status_code=499)
         final_output = request_output
