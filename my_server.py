8a67873e4b79f29d5e90c30a5e927bf714ccf359
enrique-dealba <enrique.dealba@e-o.solutions>
2023-12-18 14:40:14 -0600

    Update my_server.py
    Undo changes for quick test.

--- my_server.py
+++ my_server.py
@@ -1,73 +1,61 @@
+import argparse
 import json
 import os
-from typing import AsyncGenerator
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, Response, StreamingResponse
-from vllm import SamplingParams
+from vllm import EngineArgs, LLMEngine, SamplingParams
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.utils import random_uuid
 
-from config import DEFAULT_MODEL, MAX_TOKENS, NUM_RESPONSES, TEMPERATURE, TOP_P
-
 app = FastAPI()
 
-GPU_MEMORY_UTILIZATION = 0.30
+opt_model: str = "facebook/opt-125m"
+mistral_model: str = "mistralai/Mistral-7B-Instruct-v0.1"
+mistral_cpu: str = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF" # Doesn't work
+mistral_awq: str = "TheBloke/Mistral-7B-Instruct-v0.1-AWQ"
 
-def create_engine(model_name: str) -> AsyncLLMEngine:
-    engine_args = AsyncEngineArgs(model=model_name,
-                                  gpu_memory_utilization=GPU_MEMORY_UTILIZATION)
-    return AsyncLLMEngine.from_engine_args(engine_args)
+zephyr_model: str = "HuggingFaceH4/zephyr-7b-beta"
+hermes_model: str = "teknium/OpenHermes-2.5-Mistral-7B"
 
-model = os.getenv("MODEL", DEFAULT_MODEL)
-engine = create_engine(model)
+# Setting model directly
+llm_model = os.getenv("MODEL", mistral_model)
 
-# Request Handlers
-async def parse_request(request: Request) -> str:
-    request_dict = await request.json()
-    return request_dict.pop("text", None)
+# gpu_memory_utilization=0.25 works for 7B models
+engine_args = AsyncEngineArgs(model=llm_model, gpu_memory_utilization=0.30)
+engine = AsyncLLMEngine.from_engine_args(engine_args)
 
-def create_sampling_params() -> SamplingParams:
-    return SamplingParams(n=NUM_RESPONSES,
-                          temperature=TEMPERATURE,
-                          top_p=TOP_P,
-                          max_tokens=MAX_TOKENS,
-    )
-
-async def stream_results(generator: AsyncGenerator) -> AsyncGenerator[bytes, None]:
-    async for request_output in generator:
-        prompt = request_output.prompt
-        text_outputs = [prompt + output.text for output in request_output.outputs]
-        ret = {"text": text_outputs}
-        yield (json.dumps(ret) + "\0").encode("utf-8")
-
-async def get_final_output(generator: AsyncGenerator, request_id: str, request: Request):
-    async for request_output in generator:
-        if await request.is_disconnected():
-            print("Client disconnected. Aborting request.")
-            await engine.abort(request_id)
-            return None
-        return request_output
-
-# FastAPI Routes
 @app.post("/generate")
 async def generate(request: Request):
-    prompt = await parse_request(request)
-    sampling_params = create_sampling_params()
+    request_dict = await request.json()
+    prompt = request_dict.pop("text", None)
+
+    sampling_params = SamplingParams(n=1, # Number of output sequences to return for the given prompt
+                              temperature=0.2,
+                              top_p=0.95, # Must be in (0, 1] - set to 1 to consider all tokens
+                              #frequency_penalty=0.1, # > 0 leads to new tokens, < 0 leads to repeat tokens
+                              #use_beam_search=False, # whether to use beam search instead of sampling
+                              max_tokens=500,
+    )
+
     request_id = random_uuid()
     results_generator = engine.generate(prompt, sampling_params, request_id)
 
-    # TODO: Test streaming feature
-    stream = False
-    if stream:
-        return StreamingResponse(stream_results(results_generator))
-    
-    final_output = await get_final_output(results_generator, request_id, request)
-    if final_output is None:
-        return Response(status_code=499)
-    
+    # Non-streaming case
+    final_output = None
+    async for request_output in results_generator:
+        if await request.is_disconnected():
+            print("Client disconnected. Aborting request.")
+            # Abort the request if the client disconnects.
+            await engine.abort(request_id)
+            return Response(status_code=499)
+        final_output = request_output
+    # TODO: Test streaming
+
+    assert final_output is not None
     prompt = final_output.prompt
     text_outputs = [output.text for output in final_output.outputs]
     ret = {"text": text_outputs}
+
     return JSONResponse(ret)
