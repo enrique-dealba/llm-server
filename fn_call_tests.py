f45c6b92a521988434e5bf487e9cfc094819cacd
enrique-dealba <enrique.dealba@e-o.solutions>
2024-02-26 15:58:52 -0600

    Update fn_call_tests.py
    Quick Fix

--- fn_call_tests.py
+++ fn_call_tests.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 from client import Client
 from text_processing import TextProcessing as tp
-from tools.tools import get_time
+from tools.router_tools import get_time
 
 # Loads environment variables
 load_dotenv()
