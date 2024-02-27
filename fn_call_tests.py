d5906a19e259b090f9da1496950de3202c562e3c
enrique-dealba <enrique.dealba@e-o.solutions>
2024-02-26 20:48:27 -0600

    Update fn_call_tests.py
    Major code cleanup

--- fn_call_tests.py
+++ fn_call_tests.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from typing import Callable, Dict, List
 
@@ -26,6 +27,15 @@ class FunctionTest:
         self.targets = targets
 
 
+def check_response(response, expected):
+    """Boolean logic to check if function call response is coorect."""
+    check = bool(str(expected) in str(response) or str(response) in str(expected))
+    double_check = bool(
+        str(expected) in str(response)[1:-1] or str(response)[1:-1] in str(expected)
+    )
+    return check or double_check
+
+
 def function_call(fn_test: FunctionTest, num_tests: int = 20) -> Dict[str, float]:
     """Runs a series of prompts through the LLM router and benchmarks function call."""
     total_tps = 0.0
@@ -37,9 +47,13 @@ def function_call(fn_test: FunctionTest, num_tests: int = 20) -> Dict[str, float
     for i in range(num_tests):
         for idx in range(len(fn_test.prompts)):
             prompt = fn_test.prompts[idx]
-            t_0 = time.perf_counter()
-            response = Client.generate_text(prompt)
-            t_1 = time.perf_counter()
+            response = None
+            try:
+                t_0 = time.perf_counter()
+                response = Client.generate_text(prompt)
+                t_1 = time.perf_counter()
+            except Exception as e:
+                logging.error(f"LLM failed to generate text: {e}")
 
             if response and "text" in response:
                 # TODO: More elegant way to check str type
@@ -55,21 +69,14 @@ def function_call(fn_test: FunctionTest, num_tests: int = 20) -> Dict[str, float
 
                 target = fn_test.targets[idx]
                 expected_response = fn_test.function(target)
-                check = bool(
-                    str(expected_response) in str(response)
-                    or str(response) in str(expected_response)
-                )
-                double_check = bool(
-                    str(expected_response) in str(response)[1:-1]
-                    or str(response)[1:-1] in str(expected_response)
-                )
-                total_correct += int(check or double_check)  # Adds 1 if correct
+                correct = check_response(response, expected_response)
+                total_correct += int(correct)  # Adds 1 if correct
 
                 if i == 0:  # We only print these out during first iter.
                     print(f"Prompt: {prompt}")
                     print(f"Response: {response}")
                     print(f"Actual: {expected_response}")
-                    print(f"Check: {check}\n")
+                    print(f"Check: {correct}\n")
                     print(f"TPS: {tps:.2f}")
             else:
                 total_requests += 1
