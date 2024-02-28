9075ffae5fc764edf29fe7d9aea38c06e6df80b4
enrique-dealba <enrique.dealba@e-o.solutions>
2024-02-28 15:25:35 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """Config settings for LLMs and server parameters."""
 
 # ----- LLM -----
-DEFAULT_MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
+DEFAULT_MODEL = "mistralai/Mistral-7B-Instruct-v0.1"
 
 # ----- Constants -----
 NUM_GPUS = 1
