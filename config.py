b5ee53577199356811d61401aca3c8055bcb9f11
enrique-dealba <enrique.dealba@e-o.solutions>
2024-04-11 13:54:20 -0500

    Update config.py
    Testing Hermes Pro 2

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 # ----- Config Settings -----
 class Settings(BaseSettings):
     # ----- LLM -----
-    DEFAULT_MODEL: str = "TheBloke/OpenHermes-2.5-Mistral-7B-GPTQ"
+    DEFAULT_MODEL: str = "NousResearch/Hermes-2-Pro-Mistral-7B"
 
     # ----- Constants -----
     NUM_GPUS: int = 1
