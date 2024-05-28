5b4ba83a876563946aaffd0e2398f48af81f3f2d
enrique-dealba <enrique.dealba@e-o.solutions>
2024-05-28 14:38:37 -0500

    Debugging Mistral-AWQ

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 # ----- Config Settings -----
 class Settings(BaseSettings):
     # ----- LLM -----
-    DEFAULT_MODEL: str = "TheBloke/OpenHermes-2.5-Mistral-7B-GPTQ"
+    DEFAULT_MODEL: str = "TheBloke/Mistral-7B-Instruct-v0.2-AWQ"
 
     # ----- Constants -----
     NUM_GPUS: int = 1
