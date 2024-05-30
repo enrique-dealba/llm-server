74bee8b26043e7eb5ff9f3b02feb41ba2b14f19c
enrique-dealba <enrique.dealba@e-o.solutions>
2024-05-29 20:48:26 -0500

    Optics

--- run_json_tests.py
+++ run_json_tests.py
@@ -198,8 +198,8 @@ def main():
         # "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ",
         # "TheBloke/Mistral-7B-Instruct-v0.2-AWQ",
         # "teknium/OpenHermes-2.5-Mistral-7B",
-        # "TheBloke/OpenHermes-2.5-Mistral-7B-GPTQ",
-        "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ",
+        "TheBloke/OpenHermes-2.5-Mistral-7B-GPTQ",
+        # "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ",
         # "NousResearch/Hermes-2-Pro-Mistral-7B",
     ]
 
