c828c78d25fab710fa9ce51c0d739651e07a160a
enrique-dealba <140011964+enrique-dealba@users.noreply.github.com>
2024-06-07 11:53:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 # ----- Config Settings -----
 class Settings(BaseSettings):
     # ----- LLM -----
-    DEFAULT_MODEL: str = "teknium/OpenHermes-2.5-Mistral-7B"
+    DEFAULT_MODEL: str = "mistralai/Mistral-7B-Instruct-v0.1"
 
     # ----- Constants -----
     NUM_GPUS: int = 1
